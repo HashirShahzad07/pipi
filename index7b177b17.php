69cf0962fc8cb37ebdb943da952300493525556e
ark <ijustwanttobemee@gmail.com>
2024-04-23 23:18:02 +0500

    first commit

---
+++ index7b177b17.php
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! - Open Source Content Management" -->
+<feed xmlns="http://www.w3.org/2005/Atom" xml:lang="en-gb">
+	<title type="text">Tump on Base</title>
+	<subtitle type="text">Join the king of crypto, TRUMP, on his latest venture in to the base cryptosphere. Get ready to MAKE BASE GREAT AGAIN!</subtitle>
+	<link rel="alternate" type="text/html" href="https://trumponbase.com"/>
+	<id>https://trumponbase.com/index.php</id>
+	<updated>2024-04-06T00:26:07+00:00</updated>
+	<generator uri="https://www.joomla.org">Joomla! - Open Source Content Management</generator>
+	<link rel="self" type="application/atom+xml" href="https://trumponbase.com/index.php?format=feed&amp;type=atom"/>
+</feed>
---
+++ indexc0d0c0d0.php
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! - Open Source Content Management" -->
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+	<channel>
+		<title>Tump on Base</title>
+		<description><![CDATA[Join the king of crypto, TRUMP, on his latest venture in to the base cryptosphere. Get ready to MAKE BASE GREAT AGAIN!]]></description>
+		<link>https://trumponbase.com/index.php</link>
+		<lastBuildDate>Sat, 06 Apr 2024 00:26:06 +0000</lastBuildDate>
+		<generator>Joomla! - Open Source Content Management</generator>
+		<atom:link rel="self" type="application/rss+xml" href="https://trumponbase.com/index.php?format=feed&amp;type=rss"/>
+		<language>en-gb</language>
+	</channel>
+</rss>
